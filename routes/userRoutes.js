79b9faad41c88d272e57cce769e781f2e1b7b74e
mohammedelbaoussi <84717537+mohammedelbaoussi@users.noreply.github.com>
2023-09-23 19:37:18 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { register, login, logout,refreshToken, getAlluser, updateUserRole, getSingleUser } = require('../controllers/userCtrl')
+const { register, login, logout,refreshToken, getAlluser, updateUserRole, getSingleUser } = require('../controllers/UserCtrl')
 const { requireSignIn, adminMiddleware } = require('../middelwares')
 
 
